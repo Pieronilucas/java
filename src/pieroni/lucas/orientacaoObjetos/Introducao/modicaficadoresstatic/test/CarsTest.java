fce85631d2b5279eb4dc8fc048192821bbec94b0
Lucas Pieroni <lucaspieroni819918@outlook.com>
2025-02-01 14:23:25 -0300

    static method and initializer blocks

---
+++ src/pieroni/lucas/orientacaoObjetos/Introducao/modicaficadoresstatic/test/CarsTest.java
@@ -0,0 +1,18 @@
+package pieroni.lucas.orientacaoObjetos.Introducao.modicaficadoresstatic.test;
+
+import pieroni.lucas.orientacaoObjetos.Introducao.modicaficadoresstatic.domain.Car;
+
+public class CarsTest {
+    public static void main(String[] args) {
+        Car.setSpeedLimit(180);
+
+                Car car = new Car("Porsche 911", 330);
+        Car car2 = new Car("Mercedes g29", 270);
+        Car car3 = new Car("BMW M5 Competition", 320);
+
+        car.printInfo();
+        car2.printInfo();
+        car3.printInfo();
+
+    }
+}
